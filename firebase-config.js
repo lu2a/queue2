8c0ecd95ab2dc4a9a8947d7b09725429723d56f9
lu2a <62076830+lu2a@users.noreply.github.com>
2025-11-29 15:12:06 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
-// Firebase Configuration for Queue Management System
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID",
-    measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyCIkmvExsamNR8gNAJh1IJEWT4TQByLhLs",
+  authDomain: "queue2-3d5df.firebaseapp.com",
+  databaseURL: "https://queue2-3d5df-default-rtdb.firebaseio.com",
+  projectId: "queue2-3d5df",
+  storageBucket: "queue2-3d5df.firebasestorage.app",
+  messagingSenderId: "53999496018",
+  appId: "1:53999496018:web:713c271b37cb4233e0a88b",
+  measurementId: "G-RNJTFQ4CMD"
 };
 
 // Initialize Firebase
